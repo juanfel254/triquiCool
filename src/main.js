a00b005154449214a09e05f9a983655f616991cb
juanfel254 <jparrar@unal.edu.co>
2022-12-31 21:03:39 -0500

    First functional triqui board finished

--- src/main.js
+++ src/main.js
@@ -2,15 +2,34 @@ const circleFigure = "/media/pictures/circle.png"
 const exFigure = "/media/pictures/x.png"
 
 const cells = document.getElementsByTagName("td")
+const movesRecord = []
+const gameOver = false
 
-function identifyCellClicked(event) {
+function identifyCellClicked(event){
     cellId = event.target.id.toString()
+    //movesRecord.push(cellId)
     cellImgQuery = `tr.row td#${cellId} img`
-    console.log(document.querySelector(cellImgQuery))
-    document.querySelector(cellImgQuery).src = exFigure
+    insertFigure()
+}
+
+function insertFigure() {
+    let figure = ""
+    if (movesRecord.length%2 == 0){
+        figure = circleFigure
+    }
+    else {
+        figure = exFigure
+    }
+    movesRecord.push([cellId, figure])
+    console.log(movesRecord)
+    document.querySelector(cellImgQuery).src = figure
 }
 
 for (let cell of cells){
-    cell.addEventListener("click", identifyCellClicked)
+    if (gameOver != true) {
+        cell.addEventListener("click", identifyCellClicked)
+    }
+    else {
+        alert("GAME OVER")
+    }
 }
-
