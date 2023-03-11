f809966350e7f362fe7fd43b0523743a7ccce32f
juanfel254 <jparrar@unal.edu.co>
2023-03-11 15:24:22 -0500

    Styles added to board

--- src/components/game-board/board.js
+++ src/components/game-board/board.js
@@ -3,13 +3,12 @@ import styles from "@/styles/board.module.css";
 export default function Board({ moves, setMoves }) {
   return (
     <div className={styles.boardContainer}>
-      <p>I could put some info here</p>
-      <table>
+      <table className={styles.table}>
         <tbody>
           {[1,2,3].map(row => 
-            <tr className="row" key={row}>
+            <tr className={styles.row} key={row}>
               {moves.slice(-3 + row*3,row*3).map((move, index) => 
-                <td className="cell" key={index} onClick={setMoves(moves[index + 3*row - 3] = "X")}>{move}</td>
+                <td className={styles.cell} key={index}>{move}</td>
                 )}
             </tr>
           )}
